e4a36f422517d8ef27557b111f9b4d5345c99055
Paul Samsotha <87725522+psamsotha-ss@users.noreply.github.com>
2021-09-08 10:39:26 -0700

    Add order producer
    * Add order producer
    * Update MySQL model

---
+++ test/helpers/ids.py
@@ -0,0 +1,20 @@
+
+from app.db.database import Database
+
+
+def get_ids(db: Database, query: str):
+    results = db.run_query(query)
+    return list(map(lambda result: result[0], results))
+
+
+def get_user_ids(db: Database, user_role):
+    return get_ids(db, f"SELECT userId FROM user WHERE userRole = '{user_role}'")
+
+
+def get_driver_ids(db: Database):
+    return get_ids(db, "SELECT driverId FROM driver")
+
+
+def get_address_ids(db: Database):
+    return get_ids(db, "SELECT addressId FROM address")
+
